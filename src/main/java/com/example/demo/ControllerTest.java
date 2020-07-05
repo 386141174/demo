9dfe22e69164b0d4b1a56b8e346ab49f9f7b9a82
SRHW180389 <386141174@qq.com>
2020-07-05 22:22:48 +0800

    add

--- src/main/java/com/example/demo/ControllerTest.java
+++ src/main/java/com/example/demo/ControllerTest.java
@@ -6,7 +6,10 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 
 @Controller
@@ -30,6 +33,25 @@ public class ControllerTest {
 //        user.setName("aaaaa");
 //        model.addAttribute("message",user);
         return "index";
+    }
+
+    @RequestMapping("/table")
+    public String table() {
+        return "table";
+    }
+
+    @RequestMapping("/table/msg")
+    @ResponseBody
+    public Map<String,Object> tableMsg(Model model) {
+        Map<String,String> userMap = new HashMap<>();
+        userMap.put("id","001");
+        userMap.put("username","admin");
+        List<Map> list = new ArrayList<>();
+        list.add(userMap);
+        Map<String,Object> response = new LinkedHashMap<>();
+        response.put("code","0");
+        response.put("data",list);
+        return response;
     }
 
 }
